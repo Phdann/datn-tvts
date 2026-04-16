887f7907af1030678ccd9277058a317cdec36447
Phdann <phdan0110@gmail.com>
2026-04-16 19:52:05 +0700

    Cập nhật cấu hình MySQL SSL để tương thích với Aiven Cloud và hỗ trợ DB_PORT

--- server/config/database.js
+++ server/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME || 'tuyensinh_db',
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: false
   },
@@ -14,6 +15,7 @@ module.exports = {
     password: process.env.DB_PASS || '',
     database: process.env.DB_NAME_TEST || 'tuyensinh_db_test',
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: false
   },
@@ -22,8 +24,14 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: process.env.DB_DIALECT || 'mysql',
-    logging: false
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   }
 };
 
